e47dbfbebadeef05bb1dfbca4dc6ebcfca352afd
Rubén Béjar <rbejar@unizar.es>
2020-11-30 21:51:20 +0100

    Simplest travis and karma conf that should work, I hope.

--- karma.conf.js
+++ karma.conf.js
@@ -27,12 +27,6 @@ module.exports = function (config) {
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true,
-    customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    }
+    restartOnFileChange: true
   });
 };
