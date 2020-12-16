1c788b6724fede47b69df267a6a885fa4a32d59f
Rubén Béjar <rbejar@unizar.es>
2020-12-16 12:19:05 +0100

    Configured to deploy on GitHub pages, on every build on TravisCI, connected to the API in Heroku.

---
+++ build_scripts/deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+echo
+echo "Deploying to testdeployment configuration ..."
+echo
+
+export TRAVIS_BUILD_DIR=${PWD}
+
+# This builds a project that can be deployed remotely in GitHub pages with the API where
+# the testdeployment configuration says (currently Heroku)
+ng build --configuration=testdeployment --baseHref="https://sitmun.github.io/admin-app/"
+
+if [ -n "$GITHUB_API_KEY" ]; then
+     mkdir tmp
+     cd tmp
+     git clone https://github.com/sitmun/sitmun.github.io.git
+     cd sitmun.github.io
+     cp -r "$TRAVIS_BUILD_DIR"/dist/admin-app .
+     git add admin-app/*
+     git commit -m "Deployment of the sitmun-admin-app client"
+#    # Make sure to make the output quiet, or else the API token will leak!
+#    # This works because the API key can replace your password.
+     git push -q https://$USERNAME:$GITHUB_API_KEY@github.com/sitmun/sitmun.github.io master &>/dev/null
+     cd ../..
+     rm -r -f tmp
+fi
