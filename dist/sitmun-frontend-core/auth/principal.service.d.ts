02cef1a182b165ebf788ca37c22984863fd2db67
JLucena <jlucena@nexusgeographics.com>
2021-01-12 08:35:18 +0100

    fixed bug

--- dist/sitmun-frontend-core/auth/principal.service.d.ts
+++ dist/sitmun-frontend-core/auth/principal.service.d.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { AccountService } from '../account/account.service';
 /** Principal service*/
 export declare class Principal {
