94d070e24c6d89930b35a6a52ede47bc8599744a
JLucena <jlucena@nexusgeographics.com>
2021-01-13 11:59:54 +0100

    updated environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,27 @@ export const environment = {
   production: true,
   agGridTheme: "ag-theme-balham",
   scopeTypes : ['selectType','Municipal', 'Supramunicipal', 'Total'],
-  apiBaseURL: `http://localhost:8080`  // Change this when there is a real production environment
+  apiBaseURL: `http://localhost:8080`,  // Change this when there is a real production environment
+  tasksTypes : {
+    'basic':1, 
+    'download':2,
+    'document':3,
+    'locator':4,
+    'query':5,
+    'moreInfo':6,
+    'report':7,
+    'editionWFS':8,
+    'thematic':9,
+    'extraction':10,
+    'NUMEdition':11,
+    'RELEdition':12,
+    'VISEdition':13,
+    'HIDDENEdition':14
+  },
+  languages:[
+    {id:"ca", name:"catalan"},
+    {id:"es", name:"spanish"},
+    {id:"en", name:"english"},
+  ],
+  defaultLang:"ca"
 };
--- src/environments/environment.testdeployment.ts
+++ src/environments/environment.testdeployment.ts
@@ -2,5 +2,27 @@ export const environment = {
   production: false,
   agGridTheme: "ag-theme-balham",
   scopeTypes : ['selectType','Municipal', 'Supramunicipal', 'Total'],
-  apiBaseURL: `https://sitmun-backend-core.herokuapp.com`
+  apiBaseURL: `https://sitmun-backend-core.herokuapp.com`,
+  tasksTypes : {
+    'basic':1, 
+    'download':2,
+    'document':3,
+    'locator':4,
+    'query':5,
+    'moreInfo':6,
+    'report':7,
+    'editionWFS':8,
+    'thematic':9,
+    'extraction':10,
+    'NUMEdition':11,
+    'RELEdition':12,
+    'VISEdition':13,
+    'HIDDENEdition':14
+  },
+  languages:[
+    {id:"ca", name:"catalan"},
+    {id:"es", name:"spanish"},
+    {id:"en", name:"english"},
+  ],
+  defaultLang:"ca"
 };
