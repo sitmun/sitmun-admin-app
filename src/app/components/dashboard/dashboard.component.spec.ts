5c08e6ff9f87095e68c66225cac856f6c6ab6f83
Jonatan Lucena <jlucena@NG.LOCAL>
2021-03-30 14:31:23 +0200

    fix spec bug

--- src/app/components/dashboard/dashboard.component.spec.ts
+++ src/app/components/dashboard/dashboard.component.spec.ts
@@ -1,8 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { SitmunFrontendGuiModule } from 'dist/sitmun-frontend-gui/';
 import { DashboardComponent } from './dashboard.component';
+import { ExternalConfigurationService } from 'src/app/ExternalConfigurationService';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { MaterialModule } from '../../material-module';
+import { RouterModule } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
 
 describe('DashboardComponent', () => {
+  
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
 
@@ -12,6 +22,27 @@ describe('DashboardComponent', () => {
     })
     .compileComponents();
   });
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DashboardComponent ],
+      imports : [HttpClientTestingModule, HttpClientModule, SitmunFrontendGuiModule, RouterTestingModule, MatIconTestingModule,
+         MaterialModule, RouterModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: (http: HttpClient) => {
+              return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+            },
+            deps: [HttpClient]
+            }
+        })],
+      providers: [
+        { provide: 'ExternalConfigurationService', useClass: ExternalConfigurationService }, ]
+    })
+    .compileComponents();
+  });
+
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
