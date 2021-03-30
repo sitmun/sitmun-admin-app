9fdad4a7b144afff25008c9a320ac2e063f4c2ec
Jonatan Lucena <jlucena@NG.LOCAL>
2021-03-30 17:20:49 +0200

    no message

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -10,7 +10,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
 import { MaterialModule } from './material-module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { Principal, LoginService, AccountService, AuthService } from 'dist/sitmun-frontend-core/';
+import { Principal, LoginService, AccountService, AuthService, LanguageService } from 'dist/sitmun-frontend-core/';
 import { ExternalConfigurationService } from 'src/app/ExternalConfigurationService';
 import { SitmunFrontendGuiModule } from 'dist/sitmun-frontend-gui/';
 
@@ -40,7 +40,7 @@ describe('AppComponent', () => {
         SideMenuComponent,
         ToolbarComponent
       ],
-      providers: [Principal,LoginService,AccountService,AuthService,
+      providers: [Principal,LoginService,AccountService,AuthService,LanguageService,
         { provide: 'ExternalConfigurationService', useClass: ExternalConfigurationService }, ]
     }).compileComponents();
   });
