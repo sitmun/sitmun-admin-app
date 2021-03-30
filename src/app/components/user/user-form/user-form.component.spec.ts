581e161b935528cdad302276a0785c0290d6477c
Jonatan Lucena <jlucena@NG.LOCAL>
2021-03-30 17:03:54 +0200

    no message

--- src/app/components/user/user-form/user-form.component.spec.ts
+++ src/app/components/user/user-form/user-form.component.spec.ts
@@ -4,7 +4,7 @@ import { UserFormComponent } from './user-form.component';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../../../material-module';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
-import { UserService, RoleService, TerritoryService, UserConfigurationService, CodeListService, UserPositionService } from 'dist/sitmun-frontend-core/';
+import { UserService, RoleService, TerritoryService, UserConfigurationService, CodeListService, UserPositionService,TranslationService } from 'dist/sitmun-frontend-core/';
 import { ExternalConfigurationService } from 'src/app/ExternalConfigurationService';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { SitmunFrontendGuiModule } from 'dist/sitmun-frontend-gui/';
@@ -19,7 +19,7 @@ describe('UserFormComponent', () => {
       declarations: [ UserFormComponent ],
       imports: [HttpClientTestingModule, SitmunFrontendGuiModule, RouterTestingModule,
          RouterModule.forRoot([]), MaterialModule, MatIconTestingModule],
-      providers: [UserService,RoleService, TerritoryService, UserPositionService, CodeListService,UserConfigurationService,
+      providers: [UserService,RoleService, TerritoryService, UserPositionService, CodeListService,UserConfigurationService,TranslationService,
         { provide: 'ExternalConfigurationService', useClass: ExternalConfigurationService }, ]
     })
     .compileComponents();
