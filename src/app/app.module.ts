22a9f200115c07603f3ccacfb7ff98bd75ae0e76
Jonatan Lucena <jlucena@NG.LOCAL>
2021-04-06 11:44:31 +0200

    fix bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -117,7 +117,6 @@ import { SidenavService } from './services/sidenav.service';
 import { TasksFormComponent } from './components/tasks/tasks-form/tasks-form.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { TaskFormComponent } from './components/task-form/task-form.component';
-import { SanitizeHtmlPipePipe } from './components/shared/sanitize-html-pipe/sanitize-html-pipe.pipe';
 
 
 
@@ -172,7 +171,6 @@ import { SanitizeHtmlPipePipe } from './components/shared/sanitize-html-pipe/san
     TasksFormComponent,
     DashboardComponent,
     TaskFormComponent,
-    SanitizeHtmlPipePipe
   ],
   imports: [
     BrowserModule,
