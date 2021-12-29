df48c2491f8c5349c1c5a18a5f8bbaea13d7eaba
jlucena <jlucena@nexusgeographics.com>
2021-12-29 09:56:44 +0100

    new version 29/12/2021

--- src/app/components/task-form/task-form.component.ts
+++ src/app/components/task-form/task-form.component.ts
@@ -1037,6 +1037,9 @@ export class TaskFormComponent implements OnInit {
       }
       if(values[i]['textAreaSelector']){
         currentColumnDef.cellEditor='agLargeTextCellEditor';
+        currentColumnDef.cellEditorParams= {
+          maxLength: 900000
+        }
       }
       columnResults.push(currentColumnDef);
     }
