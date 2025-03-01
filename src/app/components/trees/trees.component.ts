77dc92e6fbb37cedd37fd526657d94e9ecfefab1
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2025-03-01 19:29:45 +0100

    bug #278: Adapted Trees (partially)

--- src/app/components/trees/trees.component.ts
+++ src/app/components/trees/trees.component.ts
@@ -1,13 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+import {TreeService, Tree} from '../../frontend-core/src/lib/public_api';
+import {UtilsService} from '../../services/utils.service';
+import {Router} from '@angular/router';
+import {config} from 'src/config';
+import {Observable, Subject} from 'rxjs';
+import {MatDialog} from '@angular/material/dialog';
+import {DialogMessageComponent} from '../../frontend-gui/src/lib/public_api';
 
-import { Component, OnInit } from '@angular/core';
-import { TreeService, Tree } from '../../frontend-core/src/lib/public_api';
-import { UtilsService } from '../../services/utils.service';
-import { Router } from '@angular/router';
-import { config } from 'src/config';
-import { Observable, Subject } from 'rxjs';
-import { MatDialog } from '@angular/material/dialog';
-import { DialogMessageComponent } from '../../frontend-gui/src/lib/public_api';
- 
 @Component({
   selector: 'app-trees',
   templateUrl: './trees.component.html',
@@ -15,32 +14,32 @@ import { DialogMessageComponent } from '../../frontend-gui/src/lib/public_api';
 })
 export class TreesComponent implements OnInit {
   saveAgGridStateEvent: Subject<boolean> = new Subject<boolean>();
-  dataUpdatedEvent: Subject<boolean> = new Subject <boolean>();
+  dataUpdatedEvent: Subject<boolean> = new Subject<boolean>();
   themeGrid: any = config.agGridTheme;
   columnDefs: any[];
   gridModified = false;
 
   constructor(public dialog: MatDialog,
-    public treeService: TreeService,
-    private utils: UtilsService,
-    private router: Router,
+              public treeService: TreeService,
+              private utils: UtilsService,
+              private router: Router,
   ) {
 
   }
 
   ngOnInit() {
 
-    var columnEditBtn=this.utils.getEditBtnColumnDef();
-    columnEditBtn['cellRendererParams']= {
+    var columnEditBtn = this.utils.getEditBtnColumnDef();
+    columnEditBtn['cellRendererParams'] = {
       clicked: this.newData.bind(this)
-    }
+    };
 
 
     this.columnDefs = [
-      this.utils.getSelCheckboxColumnDef(),
       columnEditBtn,
+      this.utils.getSelCheckboxColumnDef(),
       this.utils.getIdColumnDef(),
-      this.utils.getEditableColumnDef('treesEntity.name','name')
+      this.utils.getEditableColumnDef('treesEntity.name', 'name')
     ];
 
   }
@@ -50,24 +49,27 @@ export class TreesComponent implements OnInit {
     if (this.gridModified) {
 
 
-      let result = await this.utils.showNavigationOutDialog().toPromise();
-      if(!result || result.event!=='Accept') { return false }
-      else if(result.event ==='Accept') {return true;}
-      else{
+      const result = await this.utils.showNavigationOutDialog().toPromise();
+      if (!result || result.event !== 'Accept') {
+        return false;
+      } else if (result.event === 'Accept') {
+        return true;
+      } else {
         return true;
       }
+    } else {
+      return true;
     }
-    else return true
-  }	
+  }
 
-  setGridModifiedValue(value){
-    this.gridModified=value;
+  setGridModifiedValue(value) {
+    this.gridModified = value;
   }
 
   getAllTrees = () => {
 
     return this.treeService.getAll();
-  }
+  };
 
   newData(id: any) {
     this.saveAgGridStateEvent.next(true);
@@ -77,7 +79,11 @@ export class TreesComponent implements OnInit {
   applyChanges(data: Tree[]) {
     const promises: Promise<any>[] = [];
     data.forEach(tree => {
-      promises.push(new Promise((resolve, reject) => {​​​​​​​ this.treeService.update(tree).subscribe((resp) =>{​​​​​​​resolve(true)}​​​​​​​)}​​​​​​​));
+      promises.push(new Promise((resolve) => {
+        this.treeService.update(tree).subscribe(() => {
+          resolve(true);
+        });
+      }));
       Promise.all(promises).then(() => {
         this.dataUpdatedEvent.next(true);
       });
@@ -91,24 +97,24 @@ export class TreesComponent implements OnInit {
   removeData(data: Tree[]) {
 
     const dialogRef = this.dialog.open(DialogMessageComponent);
-    dialogRef.componentInstance.title=this.utils.getTranslate("caution");
-    dialogRef.componentInstance.message=this.utils.getTranslate("removeMessage");
+    dialogRef.componentInstance.title = this.utils.getTranslate('caution');
+    dialogRef.componentInstance.message = this.utils.getTranslate('removeMessage');
     dialogRef.afterClosed().subscribe(result => {
-      if(result){
-        if(result.event==='Accept') {  
+      if (result) {
+        if (result.event === 'Accept') {
           const promises: Promise<any>[] = [];
           data.forEach(tree => {
-            promises.push(new Promise((resolve, reject) => {​​​​​​​ this.treeService.delete(tree).subscribe((resp) =>{​​​​​​​resolve(true)}​​​​​​​)}​​​​​​​));
+            promises.push(new Promise((resolve,) => {
+              this.treeService.delete(tree).subscribe(() => {
+                resolve(true);
+              });
+            }));
             Promise.all(promises).then(() => {
               this.dataUpdatedEvent.next(true);
             });
           });
-       }
+        }
       }
     });
-
-
-
   }
-
 }
