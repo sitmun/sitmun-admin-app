0616066d2f7dd5f442b404facff40331712c0471
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2025-03-05 19:59:15 +0100

    refact: Move constants to constants.ts and reativate query form

--- src/environments/constants.ts
+++ src/environments/constants.ts
@@ -33,4 +33,173 @@ export const constants = {
       wms: 'WMS'
     }
   },
+  menus: [
+    [
+      {
+        id: 'connection',
+        icon: 'menu_connexio',
+        children: null,
+      },
+      {
+        id: 'service',
+        icon: 'menu_servei',
+        children: null,
+      },
+      {
+        id: 'layers',
+        icon: 'menu_capes',
+        children: null,
+      },
+      {
+        id: 'trees',
+        icon: 'menu_arbres',
+        children: null,
+      },
+      {
+        id: 'backgroundLayers',
+        icon: 'menu_capes_fons',
+        children: null,
+      }
+    ],
+    [
+      {
+        id: 'layersPermits',
+        icon: 'menu_permisos',
+        children: null,
+      },
+      {
+        id: 'territory',
+        icon: 'menu_territori',
+        children: null,
+      },
+      {
+        id: 'role',
+        icon: 'menu_rol',
+        children: null,
+      },
+      {
+        id: 'user',
+        icon: 'menu_usuari',
+        children: null,
+      },
+    ],
+    [
+      /*
+        {
+          id: 'taskGroup',
+          icon: 'ic_gruptasca',
+          children: null,
+        }, */
+      {
+        id: 'tasks',
+        icon: 'menu_tasques',
+        children: [
+          {
+            id: 'tasks',
+            translation: 'basics',
+            children: null,
+          },
+          /*
+          {
+            id: 'tasksDownload',
+            children: null,
+          },
+
+          {
+            id: 'tasksDocument',
+            children: null,
+          },
+          */
+          {
+            id: 'tasksQuery',
+            children: null,
+          },
+          /*
+          {
+            id: 'tasksMoreInformation',
+          },
+
+          {
+            id: 'tasksLocator',
+          },
+
+          {
+            id: 'tasksReport',
+          },
+
+          {
+            id: 'tasksEdition',
+            children: [
+              {
+                id: 'tasksEditionCartographyTable',
+                translation: 'cartographyTableWFT'
+              },
+              {
+                id: 'tasksEditionDataTable',
+                translation: 'dataTable',
+              },
+              {
+                id: 'tasksEditionRelationTable',
+                translation: 'relationTable',
+              },
+              {
+                id: 'tasksEditionSearchView',
+                translation: 'searchView',
+              },
+            ]
+          },
+
+          {
+            id: 'tasksThematic',
+          },
+
+          {
+            id: 'tasksExtractionFME',
+          },
+           */
+        ]
+      }
+    ],
+    [
+      {
+        id: 'application',
+        icon: 'menu_aplicacio',
+        children: null,
+      }
+    ]
+  ],
+  extraImg: [
+    {
+      id: 'ic_arrow_down_black',
+      icon: 'ic_arrow_down_black',
+    },
+    {
+      id: 'ic_translate',
+      icon: 'ic_translate',
+    },
+    {
+      id: 'ic_translate',
+      icon: 'ic_translate',
+    },
+    {
+      id: 'icon_lang_ca',
+      icon: 'flag_ca',
+    },
+    {
+      id: 'icon_lang_en',
+      icon: 'flag_en',
+    },
+    {
+      id: 'icon_lang_es',
+      icon: 'flag_es',
+    },
+    {
+      id: 'icon_lang_oc',
+      icon: 'flag_oc',
+    },
+    {
+      id: 'icon_lang_fr',
+      icon: 'flag_oc',
+    },
+  ]
 };
