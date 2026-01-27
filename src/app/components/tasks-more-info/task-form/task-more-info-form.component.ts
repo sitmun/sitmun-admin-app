fb3cba25cbf617a7b8d985b416e3b91603b961d7
mplanas <mplanas@nexusgeographics.com>
2026-01-27 10:47:35 +0100

    Parameters column has been recovered into more info task

--- src/app/components/tasks-more-info/task-form/task-more-info-form.component.ts
+++ src/app/components/tasks-more-info/task-form/task-more-info-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, TemplateRef, ViewChild} from "@angular/core";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {MatDialog} from "@angular/material/dialog";
 import {ActivatedRoute, Router} from "@angular/router";
@@ -7,7 +7,7 @@ import {TranslateService} from "@ngx-translate/core";
 import {firstValueFrom, of} from "rxjs";
 
 import {BaseFormComponent} from "@app/components/base-form.component";
-import {DataTableDefinition} from "@app/components/data-tables.util";
+import {DataTableDefinition, TemplateDialog} from "@app/components/data-tables.util";
 import {Configuration} from "@app/core/config/configuration";
 import {MessagesInterceptorStateService} from "@app/core/interceptors/messages.interceptor";
 import {
@@ -22,7 +22,9 @@ import {
   TaskAvailabilityService,
   TaskGroup,
   TaskGroupService,
+  TaskParameter,
   TaskProjection,
+  TaskPropertiesBuilder,
   TaskService,
   TaskType,
   TaskTypeService,
@@ -32,6 +34,7 @@ import {
 } from "@app/domain";
 import {TaskMoreInfoProperties} from "@app/domain/task/models/task-more-info.model";
 import {
+  canKeepOrUpdate,
   onCreate,
   onDelete,
   onUpdatedRelation,
@@ -55,6 +58,7 @@ export class TaskMoreInfoFormComponent extends BaseFormComponent<TaskProjection>
   
   protected readonly rolesTable: DataTableDefinition<Role, Role>;
   protected readonly availabilitiesTable: DataTableDefinition<TaskAvailabilityProjection, TerritoryProjection>;
+  protected readonly parametersTable: DataTableDefinition<TaskParameter, TaskParameter>;
   
   private taskTypeName: string = null;
   protected taskTypeNameTranslated: string = null;
@@ -69,6 +73,9 @@ export class TaskMoreInfoFormComponent extends BaseFormComponent<TaskProjection>
     {value: 'sql', label: 'tasksMoreInfoEntity.dataAccessTypeSql'},
     {value: 'url-redirect', label: 'tasksMoreInfoEntity.dataAccessTypeUrlRedirect'}
   ];
+
+  @ViewChild('newParameterDialog', {static: true})
+  private readonly newParameterDialog: TemplateRef<any>;
 
   constructor(
     dialog: MatDialog,
@@ -93,6 +100,7 @@ export class TaskMoreInfoFormComponent extends BaseFormComponent<TaskProjection>
     super(dialog, translateService, translationService, codeListService, loggerService, errorHandler, activatedRoute, router, loadingService, messagesInterceptorState);
     this.rolesTable = this.defineRolesTable();
     this.availabilitiesTable = this.defineAvailabilitiesTable();
+    this.parametersTable = this.defineParametersTable();
   }
 
   override ngOnInit(): void {
@@ -103,9 +111,10 @@ export class TaskMoreInfoFormComponent extends BaseFormComponent<TaskProjection>
     const type = magic.taskMoreInfoTypeId;
 
     this.dataTables.register(this.rolesTable)
-      .register(this.availabilitiesTable);
+      .register(this.availabilitiesTable)
+      .register(this.parametersTable);
 
-    await this.initCodeLists(['tasksEntity.type'])
+    await this.initCodeLists(['tasksEntity.type', 'taskEntity.jsonParamType'])
     this.initTranslations('Task', ['name'])
 
     const [taskTypes, taskGroups, cartographies] = await Promise.all([
@@ -287,4 +296,51 @@ export class TaskMoreInfoFormComponent extends BaseFormComponent<TaskProjection>
       .build();
   }
 
+  private defineParametersTable(): DataTableDefinition<TaskParameter, TaskParameter> {
+    return DataTableDefinition.builder<TaskParameter, TaskParameter>(this.dialog, this.errorHandler, this.loadingService)
+      .withRelationsColumns([
+        this.utils.getSelCheckboxColumnDef(),
+        this.utils.getEditableColumnDef('common.form.name', 'name'),
+        this.utils.getNonEditableColumnDef('common.form.type', 'type'),
+        this.utils.getEditableColumnDef('common.form.value', 'value', 300, 500),
+        this.utils.getStatusColumnDef()])
+      .withRelationsOrder('name')
+      .withRelationsFetcher(() => {
+        if (this.entityToEdit?.properties?.parameters) {
+          const originalParameters = this.entityToEdit.properties.parameters;
+          const parameters = originalParameters.map((parameter: any) => TaskParameter.fromObject(parameter));
+          return of(parameters);
+        }
+        return of<TaskParameter[]>([])
+      })
+      .withRelationsUpdater(async (parameters: (TaskParameter & Status)[]) => {
+        const parametersToSave = parameters.filter(canKeepOrUpdate).map(value => TaskParameter.fromObject(value))
+        this.entityToEdit.properties = TaskPropertiesBuilder.from(this.entityToEdit.properties)
+          .withParameters(parametersToSave).build();
+        await firstValueFrom(this.taskService.update(this.entityToEdit));
+      })
+      .withTemplateDialog('newParameterDialog', () => TemplateDialog.builder()
+        .withReference(this.newParameterDialog)
+        .withTitle(this.translateService.instant('entity.task.parameters.title'))
+        .withForm(new FormGroup({
+          name: new FormControl('', {
+            validators: [Validators.required],
+            nonNullable: true
+          }),
+          type: new FormControl('', {
+            validators: [Validators.required],
+            nonNullable: true
+          }),
+          value: new FormControl('', {
+            validators: [Validators.required],
+            nonNullable: true
+          })
+        })).withPreOpenFunction((form: FormGroup) => {
+          const defaultType = this.defaultValueOrNull('taskEntity.jsonParamType');
+          form.reset({type: defaultType?.value || null});
+        }).build())
+      .withTargetToRelation((items: TaskParameter[]) => items.map(item => TaskParameter.fromObject(item)))
+      .withRelationsDuplicate(item => TaskParameter.fromObject(item))
+      .build();
+  }
 }
